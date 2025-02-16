e7975f3804b2e74c03670fee27b66b3112e126ab
Ditrich79 <adk18@mail.ru>
2025-02-16 19:06:29 +0300

    Fourth report

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -5,12 +5,12 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']
-    list_filter = ['status', 'created', 'publish', 'author']
+    list_filter = ['status', 'created', 'publish', 'author']  # Фильтр справа в админке по указанным полям
     search_fields = ['title', 'body']
-    prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ['author']
+    prepopulated_fields = {'slug': ('title',)}  # Автоматическое заполнение поля слаг
+    raw_id_fields = ['author']  # Позволяет найти пользователя по id
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
-    show_facets = admin.ShowFacets.ALWAYS
+    show_facets = admin.ShowFacets.ALWAYS  # Создаёт в фильтре справа количество объектов
 
 
